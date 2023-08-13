7c21a33ab29512ed408fca769f492e86266d6483
github takaryu39 <ryuta0takamiya@gmail.com>
2023-08-13 15:42:52 +0900

    小文字→大文字を変更したディレクトリを一旦削除

--- components/base/Spinner.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from 'react'
-
-export const Spinner: FC = () => {
-  return (
-    <div className="w-8 h-8 mx-auto animate-spin rounded-full border-2 border-primary  border-t-transparent"></div>
-  )
-}
